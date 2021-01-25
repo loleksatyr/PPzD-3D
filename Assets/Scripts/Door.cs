8865ecb67c9c156fe97bd196dd7b23b92da6e6c9
loleksatyr <czarnotabartek@gmail.com>
2021-01-25 21:43:15 +0100

    lab_05 zad 1

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Events;
 
 public class Door : MonoBehaviour {
     [SerializeField] Animator animator;
     [SerializeField] UnityEvent onDoorOpen = new UnityEvent();
     [SerializeField] UnityEvent onDoorClose = new UnityEvent();
-
+    [SerializeField] AudioMixerSnapshot otuside;
+    [SerializeField] AudioMixerSnapshot inside;
+    [SerializeField] AudioSource door;
+    [SerializeField] AudioClip closedoor;
+    [SerializeField] AudioClip opendoor;
+    public static bool ifin = false;
+    
+  
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
             animator.SetBool("character_nearby", true);
             onDoorOpen.Invoke();
+           door.PlayOneShot(opendoor,1f);
+            ifin = !ifin;
+
         }
     }
 
@@ -18,6 +29,21 @@ public class Door : MonoBehaviour {
         if (other.gameObject.CompareTag("Player")) {
             animator.SetBool("character_nearby", false);
             onDoorClose.Invoke();
+            door.PlayOneShot(closedoor, 1f);
+        }
+    }
+    private void Update()
+    {
+        if (ifin)
+        {
+            inside.TransitionTo(1f);
+          
+
+        }
+        else {
+            otuside.TransitionTo(1f);
+           
+
         }
     }
 }
---
+++ Assets/enter_lab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enter_lab : MonoBehaviour
+{
+   
+           private void OnTriggerEnter(Collider other)
+    {
+        Door.ifin = true;
+    }
+}
